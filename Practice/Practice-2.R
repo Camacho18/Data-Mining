45f87251beeb3aa57bcd9ef45b32c5e180ddf79e
Camacho18 <luis.camacho@tectijuana.edu.mx>
2020-02-17 17:45:09 -0800

    Create Practice-2-> 10 funtions

---
+++ Practice/Practice-2.R
@@ -0,0 +1,11 @@
+# Practice find 20 more funtions in R and make an example of it.
+x<-c(14,15,16,20,14) # A generic function which combines its arguments 
+Sys.getenv(x)  # Get the value of the environment variable "x"
+abs(x)         # The absolute value of "x"
+append(x,14)   # Add elements to a vector
+cat(x)         # Prints the arguments
+unique(x)      # Remove duplicate entries from vector
+Sys.time()     # Return system time
+Sys.Date()     # Return system date
+getwd()        # Return working directory
+setwd("/home") # Set working directory
