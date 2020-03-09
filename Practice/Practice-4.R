07242bf0bd9a804449c82dc9357a73c800ffddcc
Deyanira <35790925+DeyaniraCF@users.noreply.github.com>
2020-03-09 15:49:42 -0700

    Update Practice-4.R

--- Practice/Practice-4.R
+++ Practice/Practice-4.R
@@ -29,3 +29,35 @@
 #Derrick Rose: 2007 - College Data Used
 #Derrick Rose: 2006 - Proxied With 2007 Data
 #Derrick Rose: 2005 - Proxied With 2007 Data
+
+#Seasons
+Seasons <- c("2005","2006","2007","2008","2009","2010","2011","2012","2013","2014")
+Seasons
+#Players
+Players <- c("KobeBryant","JoeJohnson","LeBronJames","CarmeloAnthony","DwightHoward","ChrisBosh","ChrisPaul","KevinDurant","DerrickRose","DwayneWade")
+Players
+#Free Throws
+KobeBryant_FT <- c(696,667,623,483,439,483,381,525,18,196)
+JoeJohnson_FT <- c(261,235,316,299,220,195,158,132,159,141)
+LeBronJames_FT <- c(601,489,549,594,593,503,387,403,439,375)
+CarmeloAnthony_FT <- c(573,459,464,371,508,507,295,425,459,189)
+DwightHoward_FT <- c(356,390,529,504,483,546,281,355,349,143)
+ChrisBosh_FT <- c(474,463,472,504,470,384,229,241,223,179)
+ChrisPaul_FT <- c(394,292,332,455,161,337,260,286,295,289)
+KevinDurant_FT <- c(209,209,391,452,756,594,431,679,703,146)
+DerrickRose_FT <- c(146,146,146,197,259,476,194,0,27,152)
+DwayneWade_FT <- c(629,432,354,590,534,494,235,308,189,284)
+#Matrix
+#
+FreeThrows <- rbind(KobeBryant_FT, JoeJohnson_FT, LeBronJames_FT, CarmeloAnthony_FT, DwightHoward_FT, ChrisBosh_FT, ChrisPaul_FT, KevinDurant_FT, DerrickRose_FT, DwayneWade_FT)
+FreeThrows
+#Remove vectors - we don't need them anymore
+rm(KobeBryant_FT, JoeJohnson_FT, CarmeloAnthony_FT, DwightHoward_FT, ChrisBosh_FT, LeBronJames_FT, ChrisPaul_FT, DerrickRose_FT, DwayneWade_FT, KevinDurant_FT)
+FreeThrows
+#Rename the columns
+colnames(FreeThrows) <- Seasons
+#Rename the rows
+rownames(FreeThrows) <- Players
+
+#Check the matrix
+FreeThrows
