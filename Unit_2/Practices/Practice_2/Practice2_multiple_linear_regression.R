da44c1d43db4ae69b2716fe84dc62c7c7cffd0da
Camacho18 <luis.camacho@tectijuana.edu.mx>
2020-06-03 16:02:25 -0500

    Create Practice_2

---
+++ Unit_2/Practices/Practice_2/Practice2_multiple_linear_regression.R
@@ -0,0 +1,61 @@
+getwd()
+setwd("C:/Users/Dell/Desktop/Semestre_X/Practica_2")
+getwd()
+
+# Importing the dataset
+dataset <- read.csv('50_Startups.csv')
+
+# Encoding categorical data 
+dataset$State = factor(dataset$State,
+                       levels = c('New York', 'California', 'Florida'),
+                       labels = c(1,2,3))
+
+dataset
+# Splitting the dataset into the Training set and Test set
+# Install.packages('caTools')
+library(caTools)
+set.seed(123)
+split <- sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set <- subset(dataset, split == TRUE)
+test_set <- subset(dataset, split == FALSE)
+
+# Fitting Multiple Linear Regression to the Training set
+#regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State)
+regressor = lm(formula = Profit ~ .,
+               data = training_set )
+
+summary(regressor)
+
+# Prediction the Test set results
+y_pred = predict(regressor, newdata = test_set)
+y_pred
+
+
+# Assigment: visualize the siple liner regression model with R.D.Spend
+
+regressor = lm(formula = Profit ~ R.D.Spend,
+               data = training_set )
+
+summary(regressor)
+
+y_pred = predict(regressor, newdata = test_set)
+y_pred
+
+# Visualising the Training set results
+library(ggplot2)
+ggplot() +
+  geom_point(aes(x=training_set$R.D.Spend, y=training_set$Profit),
+             color = 'red') +
+  geom_line(aes(x = training_set$R.D.Spend, y = predict(regressor, newdata = training_set)),
+            color = 'blue') 
+
+
+# Visualising the Test set results
+ggplot() +
+  geom_point(aes(x=test_set$R.D.Spend, y=test_set$Profit),
+             color = 'red') +
+  geom_line(aes(x = training_set$R.D.Spend, y = predict(regressor, newdata = training_set)),
+            color = 'blue')
+  
+
+
